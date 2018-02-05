69374ada356b8ad615e23475929d6ea97cb98df3
huhai <key.hu@bindo.com>
2018-02-05 20:06:31 +0800

    no message

--- BEA/transaction_data.go
+++ BEA/transaction_data.go
@@ -21,28 +21,28 @@ type BatchTotals struct {
 }
 
 type TransactionData struct {
-	TransId           string            `json:"trans_id"`                //流水号--------11
-	AcquireTransID    string            `json:"acq_trans_id"`            //收单行交易号---37
-	TransDate         string            `json:"trans_date"`              //交易日期------13
-	TransTime         string            `json:"trans_time"`              //交易时间------12
-	Amount            string            `json:"amount"`                  //授权金额------04
-	TipAmount         string            `json:"tip"`                     //消费金额
-	Pin               string            `json:"pin"`                     //联机PINBLOCK--52
-	Pan               string            `json:"pan"`                     //主账号--------02
-	PanSeqNo          string            `json:"pan_seq_no,omitempty"`    //卡片序列号-----
-	CardExpireDate    string            `json:"card_exp_date"`           //有效期--------14
-	Track1            string            `json:"track1,omitempty"`        //磁道一--------
-	Track2            string            `json:"track2"`                  //磁道二--------35
-	PosEntryMode      EntryMode         `json:"pos_entry_mode"`          //刷卡方式------22
-	IccRelatedData    map[string]string `json:"icc_related_data"`        //IC卡相关数据--
-	AuthCode          string            `json:"auth_code"`               //授权码-------38
-	ResponseCode      BEACode           `json:"response_code"`           //响应码-------39
-	Invoice           string            `json:"invoice,omitempty"`       //发票号-------62
-	Batchtotals       BatchTotals       `json:"batch_total,omitempty"`   //settlement总金额----62
-	BatchNumber       string            `json:"batch_number,omitempty"`  //settlement批次------60
-	OriginalAmount    string            `json:"origin_amount,omitempty"` //原交易金额----
-	OriginalTransType TransactionType   `json:"origin_amount,omitempty"` //原交易金额----
-	TransType         TransactionType   `json:"trans_type,omitempty"`    //交易类型------
+	TransId           string            `json:"trans_id"`                    //流水号--------11
+	AcquireTransID    string            `json:"acq_trans_id"`                //收单行交易号---37
+	TransDate         string            `json:"trans_date"`                  //交易日期------13
+	TransTime         string            `json:"trans_time"`                  //交易时间------12
+	Amount            string            `json:"amount"`                      //授权金额------04
+	TipAmount         string            `json:"tip"`                         //消费金额
+	Pin               string            `json:"pin"`                         //联机PINBLOCK--52
+	Pan               string            `json:"pan"`                         //主账号--------02
+	PanSeqNo          string            `json:"pan_seq_no,omitempty"`        //卡片序列号-----
+	CardExpireDate    string            `json:"card_exp_date"`               //有效期--------14
+	Track1            string            `json:"track1,omitempty"`            //磁道一--------
+	Track2            string            `json:"track2"`                      //磁道二--------35
+	PosEntryMode      EntryMode         `json:"pos_entry_mode"`              //刷卡方式------22
+	IccRelatedData    map[string]string `json:"icc_related_data"`            //IC卡相关数据--
+	AuthCode          string            `json:"auth_code"`                   //授权码-------38
+	ResponseCode      BEACode           `json:"response_code"`               //响应码-------39
+	Invoice           string            `json:"invoice,omitempty"`           //发票号-------62
+	Batchtotals       BatchTotals       `json:"batch_total,omitempty"`       //settlement总金额----62
+	BatchNumber       string            `json:"batch_number,omitempty"`      //settlement批次------60
+	OriginalAmount    string            `json:"origin_amount,omitempty"`     //原交易金额----
+	OriginalTransType TransactionType   `json:"origin_trans_type,omitempty"` //原交易金额----
+	TransType         TransactionType   `json:"trans_type,omitempty"`        //交易类型------
 }
 
 func (t TransactionData) FormJson() string {
