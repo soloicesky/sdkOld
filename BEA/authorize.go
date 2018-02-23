b163401b6ac2d628f8696b00b84644f38e7f3d35
liuzaixing <liuzaixing@jollytech.com.cn>
2018-02-23 17:55:45 +0800

    预授权调整预授权完成加上msd的posentrymode

--- BEA/authorize.go
+++ BEA/authorize.go
@@ -68,6 +68,8 @@ func (auth *AuthorizeTransaction) SetFields() {
 		de22 = "80"
 	case WAVE:
 		de22 = "07"
+	case MSD:
+		de22 = "91"
 	}
 
 	if len(auth.transData.Pin) > 0 {
--- BEA/preauthCompletion.go
+++ BEA/preauthCompletion.go
@@ -59,6 +59,8 @@ func (authcmp *CompletionTransaction) SetFields() {
 		de22 = "80"
 	case WAVE:
 		de22 = "70"
+	case MSD:
+		de22 = "91"
 	}
 
 	de22 += "2"
